6c2b2b07be913a267e3191b358d44a2579e1bfd0
Jared Murphy <jaredcmurphy@gmail.com>
2018-04-04 17:15:25 -0400

    does not do migrations automatically

--- lib/generators/spree_print_invoice/install/install_generator.rb
+++ lib/generators/spree_print_invoice/install/install_generator.rb
@@ -1,8 +1,6 @@
 module SpreePrintInvoice
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      class_option :auto_run_migrations, type: :boolean, default: true
-
       def add_javascripts
         append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_print_invoice\n"
       end
@@ -12,7 +10,7 @@ module SpreePrintInvoice
       end
 
       def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        run_migrations = ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
         if run_migrations
           run 'bundle exec rake db:migrate'
         else
